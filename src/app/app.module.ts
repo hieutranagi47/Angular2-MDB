e63bc43e925e1d499990ea649906d366767bb0b7
Hieu Tran <quochieu.agi@gmail.com>
2017-06-24 20:19:56 +0800

    Added nvd3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NvD3Component } from 'ng2-nvd3';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NvD3Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+  options;
+  data;
+
+  ngOnInit() {
+    this.options = {
+      chart: {
+        type: 'pieChart',
+        height: 450,
+        donut: true,
+        x: function(d){return d.key;},
+        y: function(d){return d.y;},
+        showLabels: true,
+        pie: {
+          startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
+          endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
+        },
+        duration: 500,
+        legend: {
+          margin: {
+            top: 5,
+            right: 140,
+            bottom: 5,
+            left: 0
+          }
+        }
+      }
+    };
+    this.data = [
+      {
+        key: "Cumulative Return",
+        values: [
+          {
+            "label" : "JavaScript" ,
+            "value" : 50
+          } ,
+          {
+            "label" : "PHP" ,
+            "value" : 30
+          } ,
+          {
+            "label" : "HTML5/CSS3" ,
+            "value" : 80
+          } ,
+          {
+            "label" : "NodeJS" ,
+            "value" : 12
+          }
+        ]
+      }
+    ];
+  }
+
+}
